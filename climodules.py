21e81e5ebbbb38ac259cc4e14e17509d1b9e5f35
Arjo Chakravarty <arjo129@gmail.com>
2016-01-11 18:16:29 +0800

    Added new cmdlets

--- climodules.py
+++ climodules.py
@@ -1,10 +1,20 @@
 import autotest, os, json
-cmdlets = {'init': initNewProject , 'add-pkg:': addPkg}
 
-def addPkg(packagename):
-	
+cmdlets = {'init': initNewProject , 'help': help, 'version': version}
+defaultmodule = "help"
+
+def help():
+	print "Autobuilder - Project management and unit testing for C/C++"
+	print ""
+	print "To create a new project:\n\tautobuilder init"
+	print "To package as a nice gnu project:\n\tautobuilder export"
+	print "To run unit tests:\n\tautobuilder test"
+def version():
+	print "0.0.1"
 def initNewProject():
-	schema = {"projectname": raw_input('Name Of Project:')}
+	schema = {"projectname": raw_input("Name Of Project:"),
+	"lisence": raw_input("Lisence type e.g MIT:"),
+	"author": raw_input("Author's name e.g Arjo Chakravarty <arjo129@gmail.com>:"))}
 	try:
 		os.makedirs("src")
 	except OSError:
