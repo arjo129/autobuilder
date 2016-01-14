eb4e83f199ee1b1a91ea63a4c78f5eb54f6fc1cf
Arjo Chakravarty <arjo129@gmail.com>
2016-01-14 09:51:14 +0800

    moved  makefile into project object

--- project.py
+++ project.py
@@ -6,10 +6,24 @@ class Project:
         self.author = ""
         self.configuration = configuration.configuration()
         self.targets = {}
-        self.tests = []
-
+        self.Embedded = False
     def addDependency(target,name, version):
         self.configuration.addLibrary(name,version)
         self.targets[target].addLibrary(name,version)
     def generateMakefile():
-        makefile = open(makefile.wrk)
+        makefile = open("makefile.wrk")
+        for target in self.targets:
+            makefile.write(target.name+":"+target.dep2str())
+            makefile.write(target.getPreScript())
+            makefile.write("\t$(TARGETCC) "+target.getsources()+" "+target.getFlags())
+            makefile.write(target.getPostScript())
+            if not self.Embedded and target.library:
+                makefile.write(target.name+"-tests:"+target.name)
+            else:
+                makefile.write(target.name+"-tests:")
+                if target.library:
+                    makefile.write("\t$(HOSTCC) "+target.getsources()+" "+target.getFlags())
+                else:
+                    for test in target.getTests():
+                        makefile.write("\t$(HOSTCC) "+test+" "+target.getsources()+" "+target.getFlags())
+            makefile.write(target.name)
