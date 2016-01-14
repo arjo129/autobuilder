e3d4276b69dab9d9763b3f1b93e410dbda3ec4f4
Arjo Chakravarty <arjo129@gmail.com>
2016-01-14 10:38:05 +0800

    updated template

---
+++ target.py
@@ -0,0 +1,21 @@
+class target:
+    def __init__(self):
+        self.name = ""
+    def addLibrary(name,version):
+        #TODO: implement
+    def dep2str():
+        return ""
+    def getPreScript():
+        return ""
+    def getPostScript():
+        return ""
+    def getsources():
+        return ""
+    def getFlags():
+        return ""
+    def getTests():
+        return ""
+    def disableMain():
+        pass
+    def enableMain():
+        pass
