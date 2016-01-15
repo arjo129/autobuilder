b66337248ebe69663b8836f3fb2564f42e45426c
Arjo Chakravarty <arjo129@gmail.com>
2016-01-15 21:11:44 +0800

    added a separate "Parser" class for code coverage and function identification

---
+++ Parser.py
@@ -0,0 +1,9 @@
+class Parser:
+    def __init__(self):
+        self.filename = ""
+        self.classes = []
+        self.functions = []
+        self.functionCalls = []
+    def scan_file(fname):
+        self.filename = fname
+        
