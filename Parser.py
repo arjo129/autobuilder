47a46113b1130b0b726c561f719d92b4f9045b12
Arjo Chakravarty <arjo129@gmail.com>
2016-01-16 18:16:21 +0800

    Added Tokenizer

--- Parser.py
+++ Parser.py
@@ -7,12 +7,14 @@ class Class:
         self.name = ""
         self.private = []
         self.public = []
+        self.docString = ""
 class Function:
     def __init__(self):
         self.arguments = []
         self.returnType = ""
         self.name = ""
         self.functionCalls = []
+        self.docString = ""
 class Parser:
     def __init__(self):
         self.filename = ""
@@ -21,3 +23,18 @@ class Parser:
         self.functionCalls = []
     def scan_file(fname):
         self.filename = fname
+        #Step 1: tokenize
+        tokens = []
+        lasttoken = ""
+        for line in open(fname):
+            tokens.push(lasttoken)
+            tokens.push("\n")
+            lasttoken = ""
+            for c in line():
+                if c in set("{}[]()/<>.*+-=\|&*^% "):
+                    tokens.push(lasttoken)
+                    tokens.push(c)
+                    lasttoken = ""
+                else:
+                    lasttoken += ""
+            
